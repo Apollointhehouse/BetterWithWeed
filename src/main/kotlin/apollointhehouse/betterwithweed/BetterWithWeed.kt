85eb49c360d1ede6f1fa89102924e926fe0798b8
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2023-12-21 16:39:05 +1300

    Added joint recipe

--- src/main/kotlin/apollointhehouse/betterwithweed/BetterWithWeed.kt
+++ src/main/kotlin/apollointhehouse/betterwithweed/BetterWithWeed.kt
@@ -3,8 +3,11 @@ package apollointhehouse.betterwithweed
 import apollointhehouse.betterwithweed.block.ModBlocks
 import apollointhehouse.betterwithweed.item.ModItems
 import net.fabricmc.api.ModInitializer
+import net.minecraft.core.item.Item
+import net.minecraft.core.item.ItemStack
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import turniplabs.halplibe.helper.RecipeHelper
 
 object BetterWithWeed: ModInitializer {
 	const val MOD_ID: String = "betterwithweed"
@@ -15,5 +18,7 @@ object BetterWithWeed: ModInitializer {
 
 		ModBlocks.register()
 		ModItems.register()
+
+		RecipeHelper.craftingManager.addShapelessRecipe(ItemStack(ModItems.Joint), ModItems.DriedBud, Item.paper, Item.paper)
     }
 }
